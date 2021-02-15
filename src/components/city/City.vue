f3ce07d56be149c5696885ec3ece1ee8262c9084
homeuser_cookie <2497430769@qq.com>
2021-02-15 16:40:16 +0800

    add serach

--- src/components/city/City.vue
+++ src/components/city/City.vue
@@ -1,16 +1,18 @@
 <template>
     <div>
         <city-header></city-header>
-        
+        <city-search></city-search>
     </div>
 </template>
 
 <script>
 import cityHeader from './components/Header'
+import citySearch from './components/Search'
 export default {
     name: 'City',
     components: {
-        cityHeader
+        cityHeader,
+        citySearch
     },
     data() {
         return {
---
+++ src/components/city/components/Search.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="search">
+        <input class="search-input" type="text" placeholder="输入城市名字或拼音">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Search',
+    data() {
+        return {
+            
+        };
+    },
+    mounted() {
+        
+    },
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/global';
+.search
+    height .72rem
+    padding 0 .1rem
+    background-color $bgc
+    .search-input
+        height: .62rem;
+        width: 100%;
+        border-radius .1rem
+        text-align center
+        padding: 0 .1rem;
+        box-sizing border-box
+</style>
