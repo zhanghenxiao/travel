c7c0e124ddb6f0fa3621fc3a6968039c3e846aef
homeuser_cookie <2497430769@qq.com>
2021-02-16 16:08:42 +0800

    city list 布局完成

---
+++ src/components/city/components/Alphabet.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <ul class="list">
+            <li>A</li>
+            <li>A</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Alphabet',
+    data() {
+        return {
+            
+        };
+    },
+    mounted() {
+        
+    },
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="stylus" scoped>
+.list
+    // 字母垂直居中 采用flex布局
+    display flex
+    flex-direction column
+    justify-content center
+    // left right 0 是控制方位的
+    position absolute
+    top 1.46rem
+    right 0
+    bottom 0
+
+    li
+        line-height .4rem
+        text-align center
+</style>
---
+++ src/components/city/components/List.vue
@@ -0,0 +1,148 @@
+<template>
+    <div class="list" ref="list">
+        <div>
+            <div class="area">
+                <div class="area-title border-topbottom">当前城市</div>
+                <div class="area-city">
+                    <div class="city-wrapper">
+                        <div class="city">北京</div>
+                    </div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="area-title border-topbottom">热门城市</div>
+                <div class="area-city">
+                    <div class="city-wrapper">
+                        <div class="city">北京</div>
+                    </div>
+                    <div class="city-wrapper">
+                        <div class="city">北京</div>
+                    </div>
+                    <div class="city-wrapper">
+                        <div class="city">北京</div>
+                    </div>
+                    <div class="city-wrapper">
+                        <div class="city">北京</div>
+                    </div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="area-title border-topbottom">A</div>
+                <ul class="item-list">
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                </ul>
+                <div class="area-title border-topbottom">A</div>
+                <ul class="item-list">
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                </ul>
+                <div class="area-title border-topbottom">A</div>
+                <ul class="item-list">
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                </ul>
+                <div class="area-title border-topbottom">A</div>
+                <ul class="item-list">
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                </ul>
+                <div class="area-title border-topbottom">A</div>
+                <ul class="item-list">
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom">阿拉尔</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+export default {
+    name: 'List',
+    data() {
+        return {
+            
+        };
+    },
+    mounted() {
+        // 使用better-scroll注意布局样式
+        let bs = new BScroll(this.$refs.list)
+    },
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="stylus" scoped>
+.border-topbottom
+    &:before
+        border-color #ccc
+    &:after
+        border-color #ccc
+.border-bottom
+    &:before
+        border-color #ccc
+.list
+// 产生上下进度条的原因是没设置overflow hidden
+    overflow hidden
+    position absolute
+    top 1.46rem
+    left 0
+    right 0
+    bottom 0
+    // background-color red
+    .area
+        .area-title
+            height .32rem
+            line-height .32rem
+            background-color #eee
+            padding 0 .2rem
+            color #666
+            font-size .26rem
+        .area-city
+            // overflow: hidden;触发BFC
+            overflow: hidden;
+            padding .1rem .6rem .1rem .1rem
+            .city-wrapper
+                float left
+                width 33.33%
+                .city
+                    margin .1rem
+                    padding .1rem 0
+                    border: .02rem solid #000
+                    text-align center
+                    border-radius .1rem
+        
+        .item-list
+            // background-color #eee
+            .item
+                padding-left .2rem
+</style>
